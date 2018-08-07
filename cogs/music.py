270660c32a268807b1e5b79b57f0ea22f269c32e
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2018-08-07 22:03:18 +0300

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -32,12 +32,7 @@ from datetime import datetime
 from utils.constants import OTHER_ERROR_EMOJI, MAYBE_EMOJI, OTHER_SUCCESS_EMOJI, BLOCKED
 
 if not discord.opus.is_loaded():
-    # the 'opus' library here is opus.dll on windows
-    # or libopus.so on linux in the current directory
-    # you should replace this with the location the
-    # opus library is located in and with the proper filename.
-    # note that on windows this DLL is automatically provided for you
-    discord.opus.load_opus('opus')
+    discord.opus.load_opus('libopus.so')
 
 ytdlopts = {
     'format': 'bestaudio/best',
