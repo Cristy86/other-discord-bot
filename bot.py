4af4b2937d46f8a9deb9c2ae1dbae0764443740a
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2018-08-07 17:07:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ description = '''PythonBot but in rewrite.'''
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('e~'), description=description)
 bot.remove_command('help')
 bot.launch_time = datetime.utcnow()
-startup_extensions = ['cogs.admin','cogs.eh','cogs.cmds','cogs.mod','cogs.info','cogs.image','cogs.music','cogs.events','jishaku','cogs.api']
+startup_extensions = ['cogs.admin','cogs.eh','cogs.cmds','cogs.mod','cogs.info','cogs.image','cogs.events','jishaku','cogs.api']
 nl4 = "------"
 
 
