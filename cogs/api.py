0ee85769666eec2b41de1c3f6b08026d0bce6f12
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2018-08-18 13:46:02 +0300

    Update api.py

--- cogs/api.py
+++ cogs/api.py
@@ -80,8 +80,8 @@ class API:
             return
 	
         try:
-	    if hex is None:
-                hex = "4b4c4f"
+	     if hex is None:
+                 hex = "4b4c4f"
 
             embed = discord.Embed(color=0x000000)
             embed.set_image(url=f"http://lemmmy.pw/osusig/sig.php?colour=hex{hex}&uname={user}&pp=1&countryrank&removeavmargin&flagshadow&flagstroke&darktriangles&onlineindicator=undefined&xpbar&xpbarhex")
